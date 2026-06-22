8ae0440e3f79da6f46b045ef74e0edb554b3aa7c
conner <cmckeone24@georgefox.edu>
2026-06-22 12:26:41 -0700

    build fix

---
+++ testThing2Code/Relics/testThing2Relic.cs
@@ -0,0 +1,19 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using testThing2.testThing2Code.Extensions;
+
+namespace testThing2.testThing2Code.Relics;
+
+/// <summary>
+/// This is the base class for your mod's relics, which is set up to load the relic's images from your mod's resources.
+/// When creating a relic, right click the Relics folder and create a new file with the Custom Relic template.
+/// This will generate a class that extends this one.
+/// You can also just create the class manually; just make sure to inherit from this class.
+/// </summary>
+public abstract class testThing2Relic : CustomRelicModel
+{
+    //testThing2/images/relics
+    public override string PackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".RelicImagePath();
+    protected override string PackedIconOutlinePath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_outline.png".RelicImagePath();
+    protected override string BigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigRelicImagePath();
+}
