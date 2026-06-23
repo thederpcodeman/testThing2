3e07e36d15006b94b2c4ec4fc19776f154d1b83b
conner <cmckeone24@georgefox.edu>
2026-06-23 09:30:25 -0700

    Finish firt prototype

--- testThing2Code/Relics/BottomlessTankard.cs
+++ testThing2Code/Relics/BottomlessTankard.cs
@@ -1,12 +1,36 @@
-﻿using MegaCrit.Sts2.Core.Entities.Relics;
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.RelicPools;
 using testThing2.testThing2Code.Relics;
 
 namespace testThing2.testThing2Code.Relics;
 
+[Pool(typeof(IroncladRelicPool))]
 public class BottomlessTankard() : testThing2Relic
 {
-    public override RelicRarity Rarity =>
-        RelicRarity.Starter;
-
+    public override List<(string, string)> Localization => new PowerLoc(
+        "Bottomless Tankard",
+        "When you would die, revive with half your max hp, and add a decay to your deck",
+        "When you would die, revive with half your max hp, and add a decay to your deck");
     
+    public override RelicRarity Rarity =>
+        RelicRarity.Common; 
+
+    public override bool ShouldDieLate(Creature creature)
+    {
+        return creature != this.Owner.Creature || Owner.Creature.MaxHp <= 1;
+    }
+    
+    public override async Task AfterPreventingDeath(Creature creature)
+    {
+        Flash();
+        Owner.Creature.SetMaxHpInternal(Owner.Creature.MaxHp / 2);
+        await CreatureCmd.Heal(creature, Math.Max(1M, (Decimal) creature.MaxHp));
+        await CardPileCmd.AddCursesToDeck(Enumerable.Repeat(ModelDb.Card<Decay>(), 1), Owner);
+    }
 }
