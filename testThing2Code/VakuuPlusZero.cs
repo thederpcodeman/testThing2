b8ea0bfdd36427a9733bf7a3a1c402e7f65c14e5
conner <cmckeone24@georgefox.edu>
2026-06-24 11:08:12 -0700

    Vakuuwork

--- testThing2Code/VakuuPlusZero.cs
+++ testThing2Code/VakuuPlusZero.cs
@@ -17,22 +17,22 @@ public class VakuuPlusZero : CustomAncientModel {
             AncientOption<BloodSoakedRose>(),
             AncientOption<WhisperingEarring>(),
             AncientOption<Fiddle>(),
-            AncientOption<TwinDice>()
+            //AncientOption<TwinDice>()
             //more relic options
         ], [
             AncientOption<PreservedFog>(),
             AncientOption<SereTalon>(),
             AncientOption<DistinguishedCape>(),
-            AncientOption<CorruptedLance>(),
-            AncientOption<EmptyGem>()
+            //AncientOption<CorruptedLance>(),
+            //AncientOption<EmptyGem>()
             //more relic options
         ], [
             AncientOption<ChoicesParadox>(),
             AncientOption<MusicBox>(),
             AncientOption<LordsParasol>(),
             AncientOption<JeweledMask>(),
-            AncientOption<VoidVial>(),
-            AncientOption<BottomlessTankard>()
+            //AncientOption<VoidVial>(),
+            //AncientOption<BottomlessTankard>()
             //more relic options
         ]
     );
@@ -45,4 +45,29 @@ public class VakuuPlusZero : CustomAncientModel {
     {
         return true;
     }
+    
+    protected override IReadOnlyList<EventOption> GenerateInitialOptions()
+    {
+        var base3pools = OptionPools.Roll(Rng, (AncientEventModel) this).Select<BaseLib.Utils.AncientOption, EventOption>((Func<BaseLib.Utils.AncientOption, EventOption>) (option => this.RelicOption(option.ModelForOption))).ToList<EventOption>();
+        List<EventOption> list4 = this.Pool4.ToList<EventOption>();
+        list4.UnstableShuffle<EventOption>(this.Rng);
+        base3pools.Add(list4[0]);
+        return base3pools;
+    }
+    
+    private IEnumerable<EventOption> Pool4
+    {
+        get
+        {
+            return (IEnumerable<EventOption>) new List<EventOption>(new EventOption[5]
+            {
+                this.RelicOption<EmptyGem>(),
+                this.RelicOption<BottomlessTankard>(),
+                this.RelicOption<TwinDice>(),
+                this.RelicOption<CorruptedLance>(),
+                this.RelicOption<VoidVial>()
+            });
+        }
+    }
+    
 }
