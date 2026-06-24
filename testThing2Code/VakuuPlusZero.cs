cd20bd3587a728ecbe7c60db100aee367cc69ca8
conner <cmckeone24@georgefox.edu>
2026-06-24 10:58:39 -0700

    vakuu to baselib

---
+++ testThing2Code/VakuuPlusZero.cs
@@ -0,0 +1,48 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Events;
+using MegaCrit.Sts2.Core.Extensions;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Events;
+using MegaCrit.Sts2.Core.Models.Relics;
+using testThing2.testThing2Code.Relics;
+
+namespace testThing2.testThing2Code;
+
+
+public class VakuuPlusZero : CustomAncientModel {
+    protected override OptionPools MakeOptionPools => new OptionPools(
+        [
+            AncientOption<BloodSoakedRose>(),
+            AncientOption<WhisperingEarring>(),
+            AncientOption<Fiddle>(),
+            AncientOption<TwinDice>()
+            //more relic options
+        ], [
+            AncientOption<PreservedFog>(),
+            AncientOption<SereTalon>(),
+            AncientOption<DistinguishedCape>(),
+            AncientOption<CorruptedLance>(),
+            AncientOption<EmptyGem>()
+            //more relic options
+        ], [
+            AncientOption<ChoicesParadox>(),
+            AncientOption<MusicBox>(),
+            AncientOption<LordsParasol>(),
+            AncientOption<JeweledMask>(),
+            AncientOption<VoidVial>(),
+            AncientOption<BottomlessTankard>()
+            //more relic options
+        ]
+    );
+    public override bool IsValidForAct(ActModel act)
+    {
+        return act.ActNumber() == 3;
+    }
+
+    public override bool ShouldForceSpawn(ActModel act, AncientEventModel? rngChosenAncient)
+    {
+        return true;
+    }
+}
