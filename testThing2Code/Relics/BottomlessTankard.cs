0a749b035246b47d73065796082c4e09525d588f
conner <cmckeone24@georgefox.edu>
2026-06-26 09:37:09 -0700

    Tankard Nerf

--- testThing2Code/Relics/BottomlessTankard.cs
+++ testThing2Code/Relics/BottomlessTankard.cs
@@ -1,11 +1,14 @@
 ﻿using BaseLib.Abstracts;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Cards;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Saves.Runs;
 using testThing2.testThing2Code.Relics;
 
 namespace testThing2.testThing2Code.Relics;
@@ -13,6 +16,10 @@ namespace testThing2.testThing2Code.Relics;
 [Pool(typeof(EventRelicPool))]
 public class BottomlessTankard() : testThing2Relic
 {
+
+    private int _uses;
+    
+    private int _usesDelay;
     public override List<(string, string)> Localization => new PowerLoc(
         "Bottomless Tankard",
         "When you would die, revive with half your max hp, and add a decay to your deck",
@@ -32,5 +39,49 @@ public class BottomlessTankard() : testThing2Relic
         Owner.Creature.SetMaxHpInternal(Owner.Creature.MaxHp / 2);
         await CreatureCmd.Heal(creature, Math.Max(1M, (Decimal) creature.MaxHp));
         await CardPileCmd.AddCursesToDeck(Enumerable.Repeat(ModelDb.Card<Decay>(), 1), Owner);
+        Uses++;
+    }
+
+    public override async Task AfterSideTurnStart(CombatSide side, IReadOnlyList<Creature> participants, ICombatState combatState)
+    {
+        foreach (var creature in participants)
+        {
+            if (creature == this.Owner.Creature)
+            {
+                if (Owner.PlayerCombatState.TurnNumber != 1)
+                {
+                    if (Uses > UsesDelay)
+                    {
+                        for (int i = 0; i < Uses - UsesDelay; i++)
+                        {
+                            await CardPileCmd.AddToCombatAndPreview<Decay>(Owner.Creature, PileType.Draw, 1, Owner);
+                        }
+
+                        UsesDelay = Uses;
+                    }
+                }
+            }
+        }
+    }
+    
+    [SavedProperty]
+    public int Uses
+    {
+        get => this._uses;
+        set
+        {
+            this.AssertMutable();
+            this._uses = value;
+        }
+    }
+    [SavedProperty]
+    public int UsesDelay
+    {
+        get => this._usesDelay;
+        set
+        {
+            this.AssertMutable();
+            this._usesDelay = value;
+        }
     }
 }
